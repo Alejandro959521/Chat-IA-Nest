29766a7aa5af1f26254198811237e73366b19453
Miguel Ojeda <cabrerasm081@gmail.com>
2026-08-14 18:55:33 -0400

    .

--- src/gpt/use-cases/pros-cons-dicusser.use-case.ts
+++ src/gpt/use-cases/pros-cons-dicusser.use-case.ts
@@ -12,7 +12,7 @@ export const prosConsDicusserUseCase = async (
 ) => {
 
     const response = await openai.chat.completions.create({
-        model: 'openai/gpt-oss-20b',
+        model: 'openai/gpt-oss-120b',
         temperature: 0.8,
         max_tokens: 500,
       
